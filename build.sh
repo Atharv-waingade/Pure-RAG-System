0269db05c70bc5b487fe817f62709c627201fd84
Atharv-waingade <waingadeatharvk@gmail.com>
2026-02-25 13:44:24 +0530

    Add build script to pre-download model

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 # Exit on error
 set -o errexit
 
+# 1. Install dependencies
 pip install -r requirements.txt
 
-# Pre-download the model to the local cache
+# 2. Pre-download the model to the local cache folder
+echo "Downloading AI model during build..."
 python -c "from sentence_transformers import SentenceTransformer; SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')"
